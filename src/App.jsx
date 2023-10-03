d5a15fc50099d4586d97eed21dfe59b9f26276fa
Meriam <132971614+msadio8@users.noreply.github.com>
2023-10-03 17:11:58 +1100

    adding starter code

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import PortfolioContainer from "./components/PortfolioContainer";
+
+// to import app.css & any css lib i use
+
+const App = () => <PortfolioContainer />;
+
+export default App;
---
+++ src/components/PortfolioContainer.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import NavTabs from './Navigation';
+import Home from './pages/Header';
+import About from './pages/AboutMe';
+import Blog from './pages/portforlio';
+import Contact from './pages/Contact';
+
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  // TODO: Add a comment describing the functionality of this method
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Blog') {
+      return <Blog />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* // TODO: Add a comment describing what we are passing as props */}
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* // TODO: Add a comment explaining what is happening on the following line */}
+      <main className="mx-3">{renderPage()}</main>
+    </div>
+  );
+}

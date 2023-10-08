c8d72db620c056b9ee4e64d0dd968daf51b4f6a7
Meriam <132971614+msadio8@users.noreply.github.com>
2023-10-08 01:02:06 -0700

    fix undefined import"

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import '../style/Navigation.css';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
@@ -9,15 +9,15 @@ const Navigation = () => {
     // Bootstrap Navbar
     <Navbar bg="dark" variant="dark" expand="lg" sticky="top" className="py-0.5 mt-0">
       <Container>
-          <Navbar.Brand as={Link} to="/meriam-react-portfolio/">Meriam Sadio</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/Meriam-Portfolio/">Meriam Sadio</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
               {/* <Nav.Link as={Link} to="meriam-react-portfolio/home" className="fs-5">Home</Nav.Link> */}
-              <Nav.Link as={Link} to="/meriam-react-portfolio/about" className="fs-5 nav-link-highlight shadow-lg">About Me</Nav.Link>
-              <Nav.Link as={Link} to="/meriam-react-portfolio/portfolio" className="fs-5 nav-link-highlight shadow-lg ">Portfolio</Nav.Link>
-              <Nav.Link as={Link} to="/meriam-react-portfolio/contact" className="fs-5 nav-link-highlight shadow-lg ">Contact</Nav.Link>
-              <Nav.Link as={Link} to="/meriam-react-portfolio/resume" className="fs-5 nav-link-highlight shadow-lg ">Resume</Nav.Link>
+              <Nav.Link as={Link} to="/Meriam-Portfolio/about" className="fs-5 nav-link-highlight shadow-lg">About Me</Nav.Link>
+              <Nav.Link as={Link} to="/Meriam-Portfolio/portfolio" className="fs-5 nav-link-highlight shadow-lg ">Portfolio</Nav.Link>
+              <Nav.Link as={Link} to="/Meriam-Porfolio/contact" className="fs-5 nav-link-highlight shadow-lg ">Contact</Nav.Link>
+              <Nav.Link as={Link} to="/Meriam-Portfolio/resume" className="fs-5 nav-link-highlight shadow-lg ">Resume</Nav.Link>
             </Nav>
         </Navbar.Collapse>
       </Container>
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,22 +1,64 @@
-import React from "react";
+// import React from "react";
+// import "../style/Project.css";
+
+// // Project component
+// function Project(props) {
+//   return (
+//     // Card container for each project
+//     <div className="card">
+//       <h5 className="card-title">{props.title}</h5>
+//       <a href={props.deployedLink} target="_blank" rel="noreferrer">
+//         <img src={props.imgSrc} className="card-img-top" alt={props.imgAlt} />
+//       </a>
+//       <div className="card-body">
+//         {/* <h5 className="card-title">{props.title}</h5> */}
+//         <p className="card-text">{props.description}</p>
+//         <a
+//           href={props.deployedLink}
+//           target="_blank"
+//           rel="noreferrer"
+//           className="btn btn-deployed"
+//         >
+//           Deployed App
+//         </a>
+//         <a
+//           href={props.githubLink}
+//           target="_blank"
+//           rel="noreferrer"
+//           className="btn btn-repo"
+//         >
+//           GitHub Repo
+//         </a>
+//       </div>
+//     </div>
+//   );
+// }
+// // Export Project
+// export default Project;
+
+// import React from "react";
+import PropTypes from "prop-types";
 import "../style/Project.css";
 
-// Project component
 function Project(props) {
   return (
-    // Card container for each project
     <div className="card">
       <h5 className="card-title">{props.title}</h5>
-      <a href={props.deployedLink} target="_blank" rel="noreferrer">
+      <a
+        href={props.deployedLink}
+        target="_blank"
+        rel="noreferrer"
+        aria-label={`Visit ${props.title} deployed app`}
+      >
         <img src={props.imgSrc} className="card-img-top" alt={props.imgAlt} />
       </a>
       <div className="card-body">
-        {/* <h5 className="card-title">{props.title}</h5> */}
         <p className="card-text">{props.description}</p>
         <a
           href={props.deployedLink}
           target="_blank"
           rel="noreferrer"
+          aria-label={`Visit ${props.title} deployed app`}
           className="btn btn-deployed"
         >
           Deployed App
@@ -25,6 +67,7 @@ function Project(props) {
           href={props.githubLink}
           target="_blank"
           rel="noreferrer"
+          aria-label={`View ${props.title} GitHub repository`}
           className="btn btn-repo"
         >
           GitHub Repo
@@ -33,5 +76,15 @@ function Project(props) {
     </div>
   );
 }
-// Export Project
+
+Project.propTypes = {
+  title: PropTypes.string.isRequired,
+  deployedLink: PropTypes.string.isRequired,
+  imgSrc: PropTypes.string.isRequired,
+  imgAlt: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  githubLink: PropTypes.string.isRequired,
+};
+
 export default Project;
+
